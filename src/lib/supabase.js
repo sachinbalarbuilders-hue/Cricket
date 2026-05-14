5c146e0f86fff689b77cf3ab59864c076b4339db
sachin mandal <sachin.balarbulders@gmail.com>
2026-05-14 19:30:56 +0530

    Update Supabase to use Legacy Anon Key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://pcipxnniexnhiafkzkjz.supabase.co';
-const supabaseAnonKey = 'sb_publishable_5Mf3pFjw1D_mgkwErQprJA_YLD2lHO4';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjaXB4bm5pZXhuaGlhZmt6a2p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NTc0NjEsImV4cCI6MjA5NDMzMzQ2MX0.hS_sKohDTHWv9mbXVZdZWavNmVioQcgk4c2XbYecQCE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
